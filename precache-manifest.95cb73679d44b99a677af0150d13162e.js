8bb5de83e24109da59768372b7fba7d33ab94306
Gurparmvir Singh <43221967+gurparmvir@users.noreply.github.com>
2020-09-14 21:43:18 -0400

    Updates

---
+++ precache-manifest.95cb73679d44b99a677af0150d13162e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc68857f5842a40be40f9bfb99c9f0a0",
+    "url": "/mypage/index.html"
+  },
+  {
+    "revision": "16a248ad6390a6de3d80",
+    "url": "/mypage/static/css/main.283ad8ef.chunk.css"
+  },
+  {
+    "revision": "c55e7cdb9e29aaa7a166",
+    "url": "/mypage/static/js/2.f4332412.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mypage/static/js/2.f4332412.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16a248ad6390a6de3d80",
+    "url": "/mypage/static/js/main.5ec5fe8e.chunk.js"
+  },
+  {
+    "revision": "4cafda355972b5eacb11",
+    "url": "/mypage/static/js/runtime-main.170412b9.js"
+  },
+  {
+    "revision": "8565105fcf429b2bf4aa173e341a11a3",
+    "url": "/mypage/static/media/Singh_resume.8565105f.pdf"
+  },
+  {
+    "revision": "4f6a9ba298044e84625facee968fc1e0",
+    "url": "/mypage/static/media/img.4f6a9ba2.jpg"
+  },
+  {
+    "revision": "f22726562c627fda6ef431faf3980302",
+    "url": "/mypage/static/media/img1.f2272656.png"
+  },
+  {
+    "revision": "baa2019c64e98f153444a6d37abd81a2",
+    "url": "/mypage/static/media/layout.baa2019c.png"
+  },
+  {
+    "revision": "2ca592ff1c6c8943ebf33e03461c1f8c",
+    "url": "/mypage/static/media/pic.2ca592ff.png"
+  },
+  {
+    "revision": "38ac71e79ddc13af733161a1161e893b",
+    "url": "/mypage/static/media/robot.38ac71e7.png"
+  }
+]);
